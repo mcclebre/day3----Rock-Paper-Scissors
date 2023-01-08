05a7ca75d5644f423157002b2bde519eebcfac43
Brevan <brev.bdawg@gmail.com>
2023-01-08 13:59:23 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function startGame() {
+
+}
+
+function setNextRound() {
+
+}
+
+function computerRandomSelection() {
+
+}
+
+function revealComputerSelection(computerSelection) {
+
+}
+
+function resetState() {
+
+}
+
+function selectAnswer(e) {
+
+}
+
+function setStatusClass(element, win) {
+
+}
+
+function clearStatusClass(element) {
+
+}
+
